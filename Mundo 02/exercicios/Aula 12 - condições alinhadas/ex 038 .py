106e9f54f42b43524677196ce496874c5a3e0cc3
Henrique Rodrigues <melohenrique480@gmail.com>
2025-07-12 22:35:35 -0300

    ajustando erros no arquivo

---
+++ Mundo 02/exercicios/Aula 12 - condições alinhadas/ex 038 .py
@@ -0,0 +1,15 @@
+'''Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
+- O primeiro valor é maior
+- O segundo valor é maior
+- Não existe valor maior, os dois são iguais'''
+
+from time import sleep
+
+print('-'*10)
+
+sleep(2)
+n1 = int(input('inserir um número: '))
+print('-'*10)
+print('computando...')
+sleep(2)
+n2 = int(input('inserir outro número: '))
